f9e35238583cba7a277c1dd6c078ff33985bef1f
Chen Dong Meng <3a732001@gm.student.ncut.edu.tw>
2020-12-02 02:14:24 +0800

    每一頁上都要有兩個按鈕，分別鏈結至另外兩頁

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -45,6 +45,13 @@
                     </svg>
                 </div>
 
+                <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+                    <ul>
+                        <li><a href ={{route('home.index')}}><span style="color: blueviolet; ">首頁</span></a></li>
+                        <li><a href ={{route('news.index')}}><span style="color: blueviolet; ">最新消息</span></a></li>
+                    </ul>
+                </div>
+
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
                         <div class="p-6">
